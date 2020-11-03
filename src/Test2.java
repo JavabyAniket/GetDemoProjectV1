ce6691b3824f794793eeac20ee7b7cb93233dbb9
Aniket Petale <Aniket Petale@idea-PC>
2020-11-03 03:23:09 -0800

    First commit.....

---
+++ src/Test2.java
@@ -0,0 +1,15 @@
+
+public class Test2 {
+	
+	int a=10;
+	public void M1() {
+		System.out.println("HELLO");
+	}
+
+	public static void main(String[] args) {
+		
+		Test2 t	= new Test2();
+		System.out.println(t.a);
+		t.M1();
+	}
+}
